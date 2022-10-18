4dc8a7f8ae9045a3447e92bd1a835f5eff5cac77
kytcook <kytcook@naver.com>
2022-10-18 22:02:43 +0900

    controller_and_productContets.jsp_connection_221018

--- src/main/java/com/meaningfarm/mall/controller/ProductController.java
+++ src/main/java/com/meaningfarm/mall/controller/ProductController.java
@@ -10,13 +10,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.meaningfarm.mall.Logic.ProductLogic;
 
 
 @Controller
-@RequestMapping("/product/*")
+@RequestMapping("/product/")
 public class ProductController {
 	Logger logger = LoggerFactory.getLogger(ProductController.class);
 	private ProductLogic productLogic = null;
@@ -28,5 +27,20 @@ public class ProductController {
 //		productList = productLogic.productList(pMap);
 //		model.addAttribute("productList", productList);
 		return "product/productList";
+	}
+
+//	@GetMapping("productList.do")
+//	public Object productList() {
+//		logger.info("ProductList 호출 성공");
+//		return "redirect:productList";
+//	}
+	
+	@GetMapping("productContents.do")
+	public String productContents(Model model, @RequestParam Map<String, Object> pMap) {
+		logger.info("ProductContents 호출 성공");
+//		List<Map<String, Object>> productList = null;
+//		productList = productLogic.productList(pMap);
+//		model.addAttribute("productList", productList);
+		return "product/productContents";
 	}
 }
